b75f99d1013475b54b6c7d41584fcf5243a97ea0
Stefan van de Vooren <Stefan.van.de.Vooren@rabobank.nl>
2019-12-16 20:26:00 +0100

    feat: add code to invoke other script from npm script

---
+++ bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const program = require("../src/index");
+program(...process.argv.slice(2));
---
+++ src/index.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+const makePathToScriptAbsolute = (input) => {
+    if (path.isAbsolute(input)) {
+        return input;
+    }
+    return path.join(path.resolve(), input);
+};
+
+const invokeFunction = (fn, args) => fn.apply(args);
+
+const isFunction = (functionToCheck) => functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+
+const makeStringRed = (value) => `\x1b[31m${value}\x1b[0m`;
+
+const assert = (value, message) => {
+    if (value === false) {
+        console.error(makeStringRed(message));
+        process.exit(1);
+    }
+};
+
+
+module.exports = (script, ...args) => {
+    const [pathToScript, fn] = script.split('#');
+    const module  = require(makePathToScriptAbsolute(pathToScript));
+
+    if(fn) {
+        assert(isFunction(module[fn]), `Could not invoke the function '${fn}' on module ${pathToScript}\n`);
+        invokeFunction(module[fn], args);
+    } else if (isFunction(module.default)) {
+        invokeFunction(module.default, args);
+    } else {
+        assert(isFunction(module), `Could not invoke the default function on module '${pathToScript}'.\nThe module does not export a default function.\n`);
+        invokeFunction(module, args);
+    }
+};
