e5bce45b4a8977e4619a2d515927e7afdfefcff9
Stefan-van-de-Vooren <svandevooren@gmail.com>
2019-12-30 21:20:36 +0100

    feat(linting): fix linting issues in files

--- src/index.test.js
+++ src/index.test.js
@@ -1,87 +1,84 @@
-const index = require('./index');
 const path = require('path');
+const index = require('./index');
 
-const makePathToModuleAbsolute = (relativePath) => {
-    return path.join(path.resolve(), relativePath);
-};
+const makePathToModuleAbsolute = (relativePath) => path.join(path.resolve(), relativePath);
 
-const getModule = (relativePath) => {
-    return require(makePathToModuleAbsolute(relativePath));
-};
+// eslint-disable-next-line import/no-dynamic-require, global-require
+const getModule = (relativePath) => require(makePathToModuleAbsolute(relativePath));
 
 const makeStringRed = (value) => `\x1b[31m${value}\x1b[0m`;
 
 jest.doMock(
-    makePathToModuleAbsolute('my-module'),
-    () => ({
-        default: jest.fn(),
-        exportedFunction: jest.fn(),
-        exportedFunctionWithArgs: jest.fn()
-    }),
-    {virtual: true},
+  makePathToModuleAbsolute('my-module'),
+  () => ({
+    default: jest.fn(),
+    exportedFunction: jest.fn(),
+    exportedFunctionWithArgs: jest.fn(),
+  }),
+  { virtual: true },
 );
 
 jest.doMock(
-    makePathToModuleAbsolute('my-module-function'),
-    () => jest.fn(),
-    {virtual: true},
+  makePathToModuleAbsolute('my-module-function'),
+  () => jest.fn(),
+  { virtual: true },
 );
 
 jest.doMock(
-    makePathToModuleAbsolute('my-module-without-default-function'),
-    () => ({}),
-    {virtual: true},
+  makePathToModuleAbsolute('my-module-without-default-function'),
+  () => ({}),
+  { virtual: true },
 );
 
 beforeEach(() => {
-    jest.resetModules();
-    jest.resetAllMocks();
+  jest.resetModules();
+  jest.resetAllMocks();
 });
 
 describe('happy flow', () => {
-    test('it should call the function returned by the module', () => {
-        index('my-module-function');
-        expect(getModule('my-module-function')).toHaveBeenCalled()
-    });
+  test('it should call the function returned by the module', () => {
+    index('my-module-function');
+    expect(getModule('my-module-function')).toHaveBeenCalled();
+  });
 
-    test('it should call the default exported function', () => {
-        index('my-module');
-        expect(getModule('my-module').default).toHaveBeenCalled()
-    });
+  test('it should call the default exported function', () => {
+    index('my-module');
+    expect(getModule('my-module').default).toHaveBeenCalled();
+  });
 
-    test('it should call the exported function', () => {
-        index('my-module#exportedFunction');
-        expect(getModule('my-module').exportedFunction).toHaveBeenCalled()
-    });
+  test('it should call the exported function', () => {
+    index('my-module#exportedFunction');
+    expect(getModule('my-module').exportedFunction).toHaveBeenCalled();
+  });
 
-    test('it should call the exported function with arguments', () => {
-        index('my-module#exportedFunctionWithArgs', 'foo', 'bar');
-        expect(getModule('my-module').exportedFunctionWithArgs).toHaveBeenCalledWith('foo', 'bar')
-    });
+  test('it should call the exported function with arguments', () => {
+    index('my-module#exportedFunctionWithArgs', 'foo', 'bar');
+    expect(getModule('my-module').exportedFunctionWithArgs).toHaveBeenCalledWith('foo', 'bar');
+  });
 });
 
 describe('unhappy flow', () => {
-    test('it should exit the process when the default function does not exist', () => {
-        const mockExit = jest.spyOn(process, 'exit').mockImplementationOnce(() => {});
-        const consoleError = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
-        try {
-            // because we mocked the process.exit function, index now throws an error
-            index('my-module-without-default-function');
-        } catch (e) {}
+  test('it should exit the process when the default function does not exist', () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementationOnce(() => {});
+    const consoleError = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
+    try {
+      // because we mocked the process.exit function, index now throws an error
+      index('my-module-without-default-function');
+    } catch (e) {} // eslint-disable-line no-empty
 
-        expect(mockExit).toHaveBeenCalledWith(1);
-        expect(consoleError).toHaveBeenCalled();
-    });
+    expect(mockExit).toHaveBeenCalledWith(1);
+    expect(consoleError).toHaveBeenCalled();
+  });
 
-    test('it should exit the process when the exported function does not exist', () => {
-        const mockExit = jest.spyOn(process, 'exit').mockImplementationOnce(() => {});
-        const consoleError = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
-        try {
-            // because we mocked the process.exit function, index now throws an error
-            index('my-module#notExcistingFunction');
-        } catch (e) {}
+  test('it should exit the process when the exported function does not exist', () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementationOnce(() => {});
+    const consoleError = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
+    try {
+      // because we mocked the process.exit function, index now throws an error
+      index('my-module#notExcistingFunction');
+    } catch (e) {} // eslint-disable-line no-empty
 
-        expect(mockExit).toHaveBeenCalledWith(1);
-        expect(consoleError).toHaveBeenCalledWith(makeStringRed(`Could not invoke the function 'notExcistingFunction' on module 'my-module'\n`));
-    });
+    expect(mockExit).toHaveBeenCalledWith(1);
+    expect(consoleError).toHaveBeenCalledWith(makeStringRed('Could not invoke the function \'notExcistingFunction\' on module \'my-module\'\n'));
+  });
 });
